767c7e89066532f685a7861b01a999bef6e658e2
Damien Esp <d.espene@gmail.com>
2020-09-11 16:49:36 +0200

    limit NumGuesser (en cours)

--- NumGuesser.go
+++ NumGuesser.go
@@ -9,13 +9,17 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
+	var help int
 	random := rand.Intn(100)
 	var user int
-
+	rules := "Le but est simple, le programme choisit un nombre aléatoire entre 0 et 100. A toi de le devinez."
 
 	for {
 		fmt.Println("Votre choix: ")
 		fmt.Scan(&user)
+		if user == help {
+			fmt.Println(rules)
+		}
 		if user < random {
 			fmt.Print("C'est plus!\n")
 		}
@@ -25,6 +29,12 @@ func main() {
 		if user == random {
 			fmt.Println("GG")
 			break
+		}
+		if user >100 {
+			fmt.Println("Number out of limits or unexpected characters find, please try again.")
+		}
+		if user < 0 {
+			fmt.Println("Number out of limits or unexpected characters find, please try again.")
 		}
 	}
 }
