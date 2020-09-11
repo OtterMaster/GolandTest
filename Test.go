6758cad881d1580770c178f22f4386ccaf9a4806
Damien Esp <d.espene@gmail.com>
2020-09-11 14:23:51 +0200

    commit 2 go

--- Test.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	var a string
-	var b string
-}
--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Test")
 	fmt.Println("test"+"2")
+	fmt.Println(false || false)
 }
