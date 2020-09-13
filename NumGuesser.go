77bd3f0dbbf394949fdd16a65c7c2fa399ad72c2
Damien Esp <d.espene@gmail.com>
2020-09-13 14:21:57 +0200

    Suppression rules

--- NumGuesser.go
+++ NumGuesser.go
@@ -9,32 +9,28 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	var help int
+
 	random := rand.Intn(100)
 	var user int
-	rules := "Le but est simple, le programme choisit un nombre aléatoire entre 0 et 100. A toi de le devinez."
 
 	for {
-		fmt.Println("Votre choix: ")
+		fmt.Println("Your choice: ")
 		fmt.Scan(&user)
-		if user == help {
-			fmt.Println(rules)
-		}
-		if user < random {
-			fmt.Print("C'est plus!\n")
-		}
-		if user > random {
-			fmt.Println("C'est moins!")
-		}
-		if user == random {
-			fmt.Println("GG")
-			break
-		}
-		if user >100 {
-			fmt.Println("Number out of limits or unexpected characters find, please try again.")
-		}
+		
 		if user < 0 {
+			fmt.Print("Number out of limits or unexpected characters find, please try again.\n")
+		} else if user < random {
+			fmt.Println("Higher than your choice!")
+		}
+		if user > 100 {
 			fmt.Println("Number out of limits or unexpected characters find, please try again.")
+		} else if user > random {
+			fmt.Println("Lower than your choice!")
+		}
+
+		if user == random {
+			fmt.Println("GJ boi!")
+			break
 		}
 	}
 }
